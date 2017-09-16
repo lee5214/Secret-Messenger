ce7138c08b72d573686eed108e10bb0043b47275
congli <admin@lee5214.com>
2017-09-16 01:27:31 -0700

    small fix

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,7 @@
 //keys.js - decide which credentials to return
 
-if (process.Env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
 } else {
-  modules.exports = require('./dev');
+  module.exports = require('./dev');
 }
-//database_user
-//52145214
--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
 };
-
-mongodb://database_user:52145214@ds139844.mlab.com:39844/emaily-lee5214-prod
